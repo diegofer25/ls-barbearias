10b58d7ffb6cec1d978562c7181ea97fbd2d28b4
Diego Lamarão <diego.lamarao92@gmail.com>
2018-10-14 22:29:03 -0300

    finish expenses

--- src/pages/expenses.vue
+++ src/pages/expenses.vue
@@ -33,167 +33,167 @@
           </span>
         </div>
         <div class="row">
-            <div class="col-12 shadow-5">
-              <q-tabs
-                color="teal" v-model="selectedTab" class="animated zoomIn"
-                v-if="filterPerMounthAndYear(getExpenses).length || (selectedMounth > new Date().getMonth() || selectedYear > new Date().getFullYear())"
+          <div class="col-12 shadow-5">
+            <q-tabs
+              color="teal" v-model="selectedTab" class="animated zoomIn"
+              v-if="filterPerMounthAndYear(getExpenses).length || (selectedMounth > new Date().getMonth() || selectedYear > new Date().getFullYear())"
+            >
+              <q-tab
+                v-for="(cat, index) in filterPerMounthAndYear(getExpenses)"
+                :key="index"
+                slot="title"
+                :name="'tab-' + (index + 1)"
+                :label="cat.label"
+                :default="index === 0" style="font-family: 'Fredericka the Great', cursive;"
+                :count="cat.expenses ? cat.expenses.length : 0"
+              />
+              <q-tab
+                slot="title" name="tab-add" icon="add_circle_outline" :default="filterPerMounthAndYear(getExpenses).length === 0"
+                v-if="selectedMounth > new Date().getMonth() || selectedYear > new Date().getFullYear()"/>
+              <q-tab-pane
+                v-for="(cat, index) in filterPerMounthAndYear(getExpenses)" :key="index"
+                :name="'tab-' + (index + 1)" class="animated fadeIn"
               >
-                <q-tab
-                  v-for="(cat, index) in filterPerMounthAndYear(getExpenses)"
-                  :key="index"
-                  slot="title"
-                  :name="'tab-' + (index + 1)"
-                  :label="cat.label"
-                  :default="index === 0" style="font-family: 'Fredericka the Great', cursive;"
-                  :count="cat.expenses ? cat.expenses.length : 0"
-                />
-                <q-tab
-                  slot="title" name="tab-add" icon="add_circle_outline" :default="filterPerMounthAndYear(getExpenses).length === 0"
-                  v-if="selectedMounth > new Date().getMonth() || selectedYear > new Date().getFullYear()"/>
-                <q-tab-pane
-                  v-for="(cat, index) in filterPerMounthAndYear(getExpenses)" :key="index"
-                  :name="'tab-' + (index + 1)" class="animated fadeIn"
-                >
-                  <div class="row">
-                    <div class="col-sm-12 col-md-10 offset-md-1">
-                      <div class="row q-pb-md justify-end">
-                         <q-btn
-                          :color="cat.fixed ? 'positive' : 'teal'" class="q-mr-sm" :outline="!cat.fixed"
-                          :loading="btnLoading" :round="btnLoading"
-                          :icon="cat.fixed ? 'gps_fixed' : 'gps_not_fixed'"
-                          @click.stop="fixCAtegory(cat)"
-                        >{{ cat.fixed ? 'Despesas Fixas' : 'Fixar despesas' }}</q-btn>
-                        <q-btn color="teal" @click.stop="actionSheet = true">
-                          <q-icon name="settings" />
+                <div class="row">
+                  <div class="col-sm-12 col-md-10 offset-md-1">
+                    <div class="row q-pb-md justify-end">
+                        <q-btn
+                        :color="cat.fixed ? 'positive' : 'teal'" class="q-mr-sm" :outline="!cat.fixed"
+                        :loading="btnLoading" :round="btnLoading"
+                        :icon="cat.fixed ? 'gps_fixed' : 'gps_not_fixed'"
+                        @click.stop="fixCAtegory(cat)"
+                      >{{ cat.fixed ? 'Despesas Fixas' : 'Fixar despesas' }}</q-btn>
+                      <q-btn color="teal" @click.stop="actionSheet = true">
+                        <q-icon name="settings" />
+                      </q-btn>
+                    </div>
+                  </div>
+                  <q-action-sheet
+                    v-model="actionSheet"
+                    @ok="onOk" :grid="!$q.platform.is.mobile"
+                    :title="'Ações da categoria ' + cat.label"
+                    :actions="[
+                      {
+                        label: 'Cadastrar despesa',
+                        icon: 'add',
+                        color: 'positive',
+                        handler: {
+                          func: addExpense,
+                          payload: index
+                        }
+                      }, {
+                        label: 'Editar despesas',
+                        icon: 'edit',
+                        color: 'teal',
+                        handler: {
+                          func: editExpense,
+                          payload: cat
+                        }
+                      }, {
+                        label: 'Deletar despesas',
+                        icon: 'delete',
+                        color: 'negative',
+                        handler: {
+                          func: deleteExpense,
+                          payload: cat
+                        }
+                      }, {},
+                      {
+                        label: 'Excluir Categoria',
+                        icon: 'delete_forever',
+                        color: 'negative',
+                        handler: {
+                          func: deleteCategory,
+                          payload: cat
+                        }
+                      }
+                    ]"
+                  />
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-10 offset-md-1">
+                    <div v-if="cat.expenses.length">
+                      <q-list highlight>
+                        <q-list-header>Despesas de {{ cat.label }}</q-list-header>
+                        <q-item v-for="(expense, expenseIndex) in cat.expenses" :key="expenseIndex">
+                          <q-item-main :label="expense.deadline ? new Date(expense.deadline).toLocaleString() : ''">
+                            <span class="q-title">{{ expense.name }}</span>
+                            <p>{{ 'R$ ' + expense.value.toFixed(2).toLocaleString() }}</p>
+                          </q-item-main>
+                          <q-item-side right>
+                            <q-btn
+                              class="q-mx-sm animated fadeInLeft"
+                              round icon="edit" color="positive"
+                              v-if="editing" @click.stop="sendExpenseToEdit(expense, index)"
+                            ></q-btn>
+                            <q-btn
+                              class="q-mx-sm animated fadeInLeft"
+                              round icon="delete" color="red"
+                              v-if="deleting" @click.stop="sendExpenseToDelete(expense, cat)"
+                            ></q-btn>
+                            <q-btn
+                              class="q-mx-sm animated fadeInLeft"
+                              round icon="close" color="grey"
+                              v-if="editing || deleting"
+                              @click.stop="cancelAction"
+                            ></q-btn>
+                            <q-item-tile color="teal" />
+                          </q-item-side>
+                        </q-item>
+                      </q-list>
+                      <div class="row q-pa-md text-right">
+                        <div class="col-12 text-orange"><span class="q-subtitle">
+                          <span class="q-pa-sm">Total de {{ cat.label }}: R$ -{{ totalize(cat.expenses).toFixed(2) }}</span>
+                        </span></div>
+                      </div>
+                    </div>
+                    <div v-else>
+                      Voce ainda não possui despesas para esta categoria cadastre uma clicando
+                      <q-btn color="positive" @click.stop="addExpense(index)">Aqui</q-btn>
+                    </div>
+                  </div>
+                </div>
+              </q-tab-pane>
+              <q-tab-pane
+                name="tab-add"
+              >
+                <div class="row">
+                  <div class="col-sm-12 col-md-8 offset-md-2">
+                    <div class="row">
+                      <div class="col-12">
+                        <q-field
+                          icon="money_off"
+                          label="Nome da categoria de despesa"
+                          helper="Informe um nome para a nova categoria de despesa"
+                          :count="10"
+                        >
+                          <q-input :max-length="20" v-model="newCategory" />
+                        </q-field>
+                      </div>
+                    </div>
+                    <div class="row justify-end">
+                      <div class="col-12">
+                        <q-btn
+                          class="q-my-md" color="teal"
+                          @click.stop="pushExpensesCategory"
+                        >
+                          Cadastrar
                         </q-btn>
                       </div>
                     </div>
-                    <q-action-sheet
-                      v-model="actionSheet"
-                      @ok="onOk" :grid="!$q.platform.is.mobile"
-                      :title="'Ações da categoria ' + cat.label"
-                      :actions="[
-                        {
-                          label: 'Cadastrar despesa',
-                          icon: 'add',
-                          color: 'positive',
-                          handler: {
-                            func: addExpense,
-                            payload: index
-                          }
-                        }, {
-                          label: 'Editar despesas',
-                          icon: 'edit',
-                          color: 'teal',
-                          handler: {
-                            func: editExpense,
-                            payload: cat
-                          }
-                        }, {
-                          label: 'Deletar despesas',
-                          icon: 'delete',
-                          color: 'negative',
-                          handler: {
-                            func: deleteExpense,
-                            payload: cat
-                          }
-                        }, {},
-                        {
-                          label: 'Excluir Categoria',
-                          icon: 'delete_forever',
-                          color: 'negative',
-                          handler: {
-                            func: deleteCategory,
-                            payload: cat
-                          }
-                        }
-                      ]"
-                    />
                   </div>
-                  <div class="row">
-                    <div class="col-sm-12 col-md-10 offset-md-1">
-                      <div v-if="cat.expenses.length">
-                        <q-list highlight>
-                          <q-list-header>Despesas de {{ cat.label }}</q-list-header>
-                          <q-item v-for="(expense, expenseIndex) in cat.expenses" :key="expenseIndex">
-                            <q-item-main :label="expense.deadline ? new Date(expense.deadline).toLocaleString() : ''">
-                              <span class="q-title">{{ expense.name }}</span>
-                              <p>{{ 'R$ ' + expense.value.toFixed(2).toLocaleString() }}</p>
-                            </q-item-main>
-                            <q-item-side right>
-                              <q-btn
-                                class="q-mx-sm animated fadeInLeft"
-                                round icon="edit" color="positive"
-                                v-if="editing" @click.stop="sendExpenseToEdit(expense, index)"
-                              ></q-btn>
-                              <q-btn
-                                class="q-mx-sm animated fadeInLeft"
-                                round icon="delete" color="red"
-                                v-if="deleting" @click.stop="sendExpenseToDelete(expense, cat)"
-                              ></q-btn>
-                              <q-btn
-                                class="q-mx-sm animated fadeInLeft"
-                                round icon="close" color="grey"
-                                v-if="editing || deleting"
-                                @click.stop="cancelAction"
-                              ></q-btn>
-                              <q-item-tile color="teal" />
-                            </q-item-side>
-                          </q-item>
-                        </q-list>
-                        <div class="row q-pa-md text-right">
-                          <div class="col-12 text-orange"><span class="q-subtitle">
-                            <span class="q-pa-sm">Total de {{ cat.label }}: R$ -{{ totalize(cat.expenses).toFixed(2) }}</span>
-                          </span></div>
-                        </div>
-                      </div>
-                      <div v-else>
-                        Voce ainda não possui despesas para esta categoria cadastre uma clicando
-                        <q-btn color="positive" @click.stop="addExpense(index)">Aqui</q-btn>
-                      </div>
-                    </div>
-                  </div>
-                </q-tab-pane>
-                <q-tab-pane
-                  name="tab-add"
-                >
-                  <div class="row">
-                    <div class="col-sm-12 col-md-8 offset-md-2">
-                      <div class="row">
-                        <div class="col-12">
-                          <q-field
-                            icon="money_off"
-                            label="Nome da categoria de despesa"
-                            helper="Informe um nome para a nova categoria de despesa"
-                            :count="10"
-                          >
-                            <q-input :max-length="20" v-model="newCategory" />
-                          </q-field>
-                        </div>
-                      </div>
-                      <div class="row justify-end">
-                        <div class="col-12">
-                          <q-btn
-                            class="q-my-md" color="teal"
-                            @click.stop="pushExpensesCategory"
-                          >
-                            Cadastrar
-                          </q-btn>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </q-tab-pane>
-                <q-inner-loading :visible="loading">
-                  <q-spinner-gears size="100px" color="teal"></q-spinner-gears>
-                </q-inner-loading>
-              </q-tabs>
-              <div class="row text-center q-ma-sm" v-if="totalizeAll(filterPerMounthAndYear(getExpenses)) > 0">
-                <div class="col-12">
-                  <span class="q-pa-md text-red q-title">Total de {{ mounths[selectedMounth - 1] }}: R$ -{{ totalizeAll(filterPerMounthAndYear(getExpenses)) }}</span>
                 </div>
+              </q-tab-pane>
+              <q-inner-loading :visible="loading">
+                <q-spinner-gears size="100px" color="teal"></q-spinner-gears>
+              </q-inner-loading>
+            </q-tabs>
+            <div class="row text-center q-ma-sm" v-if="totalizeAll(filterPerMounthAndYear(getExpenses)) > 0">
+              <div class="col-12">
+                <span class="q-pa-md text-red q-title">Total de {{ mounths[selectedMounth - 1] }}: R$ -{{ totalizeAll(filterPerMounthAndYear(getExpenses)) }}</span>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/reports.vue
+++ src/pages/reports.vue
@@ -6,74 +6,111 @@
           <span class='q-display-2 text-weight-light' style="font-family: 'Fredericka the Great', cursive;">Relatórios</span>
         </div>
         <div class="row">
-          <div class="col-xs-12 col-md-10 offset-md-1">
-            <q-card class="row justify-around shadow-3">
-              <q-field
-                class="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 q-pa-md"
-              >
-                <q-select
-                  v-model="selectedMounth"
-                  float-label="Mês"
-                  radio color="teal"
-                  :options="mounthsOptions"
-                />
-              </q-field>
-              <q-field
-                class="col-xs-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 q-pa-md"
-              >
-                <q-select
-                  v-model="selectedYear"
-                  float-label="Ano"
-                  radio color="teal"
-                  :options="yearsOptions"
-                />
-              </q-field>
-            </q-card>
+          <div class="col-12">
+            <div class="row justify-end">
+              <q-btn @click.stop="pickDate" color="teal" :loading="pickingDate" :round="pickingDate">
+                Data do relatório: {{ selectedDate.toLocaleDateString() }}
+              </q-btn>
+            </div>
           </div>
         </div>
         <div class="row">
           <div class="col-12">
+            Total no mes: {{ mounthTotal }}
           </div>
         </div>
       </div>
     </div>
+    <q-modal
+      v-model="datepicker"
+      position="right"
+      @hide="cancelPick"
+      @escape-key="cancelPick"
+    >
+      <div class="row">
+        <div class="col-12">
+          <q-datetime-picker v-model="date" type="date" @change="datepicker = !datepicker" color="teal"/>
+        </div>
+      </div>
+      <div class="row justify-around">
+        <q-btn class="q-my-sm" outline color="negative" @click.stop="datepicker = !datepicker">CANCELAR</q-btn>
+        <q-btn class="q-my-sm" color="positive" @click.stop="selectDate">CONFIRMAR</q-btn>
+      </div>
+    </q-modal>
   </q-page>
 </template>
 
 <script>
-import cache from 'src/cache'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Reports',
   computed: {
     ...mapGetters('application', [
-      'mounths'
+      'mounths',
+      'getClients',
+      'getBarbers',
+      'getServices',
+      'getPayments',
+      'getSchedules',
+      'getExpenses'
     ])
   },
   data () {
     return {
-      createYear: new Date(cache.get('user').firstLoginDate).getFullYear(),
-      selectedMounth: new Date().getMonth() + 1,
-      selectedYear: new Date().getFullYear(),
-      mounthsOptions: [],
-      yearsOptions: []
+      date: new Date(),
+      selectedDate: new Date(),
+      pickingDate: false,
+      datepicker: false,
+      mounthTotal: 0
     }
   },
   mounted () {
-    const yearsNumbers = new Date().getFullYear() - this.createYear
-    for (let i = 0; i <= yearsNumbers + 1; i++) {
-      this.yearsOptions.push({
-        label: String(this.createYear + i),
-        value: this.createYear + i
+    this.calculate()
+  },
+  methods: {
+    calculate () {
+      this.calcMounthTotal()
+    },
+
+    selectDate () {
+      if (this.compareStringDate(this.selectedDate) !== this.compareStringDate(this.date)) {
+        this.selectedDate = this.date
+        this.calculate()
+      } else {
+        this.selectedDate = this.date
+      }
+      this.datepicker = !this.datepicker
+    },
+
+    pickDate () {
+      this.datepicker = !this.datepicker
+      this.pickingDate = true
+    },
+
+    cancelPick () {
+      this.datepicker = false
+      this.pickingDate = false
+    },
+
+    compareStringDate (date, format) {
+      const d = new Date(date)
+      if (format === 'xx/xxxx') {
+        return d.getMonth() + '/' + d.getFullYear()
+      } else {
+        return d.toLocaleDateString()
+      }
+    },
+
+    calcMounthTotal () {
+      const vm = this
+      vm.mounthTotal = vm.getPayments.filter(payment => {
+        return vm.compareStringDate(payment.timestamp, 'xx/xxxx') === vm.compareStringDate(vm.selectedDate, 'xx/xxxx')
       })
+        .reduce((total, payment) => {
+          return total + payment.service.price
+        }, 0)
     }
-    this.mounthsOptions = this.mounths.map((mounth, index) => {
-      return { label: mounth, value: index + 1 }
-    })
   }
 }
 </script>
-
-<style>
-</style>
